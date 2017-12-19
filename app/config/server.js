6cd685b133c020145d155f9a6d4f48ce27ad8393
Lucas <lucasfonmiranda@gmail.com>
2017-12-19 17:07:27 -0300

    Subindo novas pastas para reestruturar a apalicacao

--- app/config/server.js
+++ app/config/server.js
@@ -1,9 +1,19 @@
 //Import express
 var express = require('express');
 
+
+//Import consign 
+var consign = require('consign');
+
+
 //Init the object express
 var app = express();
 
+consign()
+    .include('../routes')
+    .then('../models')
+    .then('../controllers')
+    .into(app);
 
 
 
