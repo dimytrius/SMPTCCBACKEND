124db999e648afc28a0beb9deb7cac9fa13d7fe2
Lucas <lucasfonmiranda@gmail.com>
2017-12-20 10:23:04 -0300

    Mudanca no mail.js agora funciona

--- app/models/mail.js
+++ app/models/mail.js
@@ -1,11 +1,14 @@
 var axios = require('axios');
+
+
 setInterval(() =>{
-    axios.get('https://www.automi-api.herokuapp.com')
-        .then((result) =>{
-            var lat = result.data.data[0].latitude
-            var long = result.data.data[0].longitude
-            console.log(lat)
-            console.log(long)
+    axios.get('http://automi-api.herokuapp.com/')
+        .then((response) =>{
+            var lat = response.data.data[0].latitude;
+            var lng = response.data.data[0].longitude;
+            console.log(lat);
+            console.log(lng);
+
             
         })
         .catch((error) =>{
