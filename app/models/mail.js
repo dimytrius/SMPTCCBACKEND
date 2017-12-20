e120e27894cde0d942a2b9ccaa17d0bd61e3bc66
dimytrius <dimy.rocha@gmail.com>
2017-12-20 13:23:03 -0300

    mail sendgrid upgrade

--- app/models/mail.js
+++ app/models/mail.js
@@ -1,34 +1,41 @@
 var axios = require('axios');
-
-
+var cont;
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 setInterval(() =>{
     axios.get('http://automi-api.herokuapp.com/')
         .then((response) =>{
             var lat = response.data.data[0].latitude;
             var lng = response.data.data[0].longitude;
+            var status = response.data.data[0].status;
+            var date = new Date();
+            var minutes = date.getMinutes();
+            var pastminutes;
+            const msg = {
+                to: 'dimy.rocha@gmail.com',
+                from: 'dimy.rocha@gmail.com',
+                subject: 'Latitude e Longitude',
+                text: 'lat'+lat +'long' + lng,
+                html: 'lat = '+lat +'long = ' + lng,
+            };
+            
+            console.log(minutes)
+            console.log(status)
+            
+            if(status=='13'){
+                //sgMail.send(msg);
+                console.log("email") 
+                   
+            }
+           
             console.log(lat);
             console.log(lng);
-
-            
-        })
+        }) 
         .catch((error) =>{
             console.log(error)
         })
             
 }, 1200);
-
-// const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// const msg = {
-//   to: 'dimy.rocha@gmail.com',
-//   from: 'dimy.rocha@gmail.com',
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// };
-// sgMail.send(msg);
-
-
 
 
 
