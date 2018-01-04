cf0828af22e2463baa3b33da2705cbd2faabe3d5
dimytrius <dimy.rocha@gmail.com>
2018-01-04 10:12:17 -0300

    auth.js hexadecimal ok

--- app/models/auth.js
+++ app/models/auth.js
@@ -42,6 +42,31 @@ var Int64 = require('int64-native');
     }
   )
 
+  function convertfloat(str) {
+    var float = 0, sign, order, mantiss,exp,
+    int = 0, multi = 1;
+    if (/^0x/.exec(str)) {
+        int = parseInt(str,16);
+    }else{
+        for (var i = str.length -1; i >=0; i -= 1) {
+            if (str.charCodeAt(i)>255) {
+                console.log('Wrong string parametr'); 
+                return false;
+            }
+            int += str.charCodeAt(i) * multi;
+            multi *= 256;
+        }
+    }
+    sign = (int>>>31)?-1:1;
+    exp = (int >>> 23 & 0xff) - 127;
+    mantiss = ((int & 0x7fffff) + 0x800000).toString(2);
+    for (i=0; i<mantiss.length; i+=1){
+        float += parseInt(mantiss[i])? Math.pow(2,exp):0;
+        exp--;
+    }
+    return float*sign;
+}
+//(convertfloat(str));
 
 
 function get_message(device_id){
@@ -79,9 +104,12 @@ function coordenat_data(device_id){
         var battery2 = a.substring(index1+29,index1+31);
         var hour = strftime(' %H:%M:%S', unixTime(new Date(hourcrip)));
         var date = strftime('%b %d, %Y', unixTime(new Date(hourcrip)));
-        //var lathex =  data.substring(4,12);
-        var lathex = 'c1b6df66';
-        var lnghex = data.substring(12,20);
+        var lathex =  "0x"+data.substring(4,12);
+        var lnghex = "0x"+data.substring(12,20);
+        console.log(lathex);
+        console.log(lnghex);
+        lathex = convertfloat(lathex);
+        lnghex = convertfloat(lnghex);
         battery = parseInt("0x"+battery);
         battery = battery * 2.44;
         battery2 = battery2 *2.44;
@@ -99,14 +127,15 @@ function coordenat_data(device_id){
         console.log("Data:" + data);
         console.log("Hour:" + hour);
         console.log("Date: " + date);
-        console.log("Lat:"+ lat);
-        console.log("lng:"+ lng);
+        
         console.log("battery:"+ battery);
         console.log("Mac:"+mac);
         console.log("battery 2006:"+ battery2);
         console.log("rssi:"+ rssi);
         console.log(device);
         */
+        console.log("Lat:"+ lathex);
+        console.log("lng:"+ lnghex);
         //firebase
    
     
@@ -126,8 +155,8 @@ function coordenat_data(device_id){
             device.set(
                 {
                   device: devicename,
-                  latitude: latstatus13,
-                  longitude: lngstatus13,
+                  latitude: lathex,
+                  longitude: lnghex,
                   hour: hour,
                   date: date,
                   status: status,
